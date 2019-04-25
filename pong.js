c2a80de812f1e91f0f534a09ef063e14d7c670d8
Chris Bryant <chris@urbandiscourse.com>
2019-04-25 14:51:11 -0600

    add event handlers for paddle

--- pong.js
+++ pong.js
@@ -52,4 +52,25 @@
     }
   }
 
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
+  function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+
   setInterval(draw, 10)
