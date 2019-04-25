f0a40e6ebf6976e35eed672116b38b1a471a9222
Chris Bryant <chris@urbandiscourse.com>
2019-04-25 14:32:25 -0600

    pull js into separate file

---
+++ pong.js
@@ -0,0 +1,55 @@
+  var canvas = document.getElementById("gameCanvas");
+  var ctx = canvas.getContext("2d");
+
+  var x = canvas.width/2;
+  var y = canvas.height-30;
+  var dx = 1.5;
+  var dy = -1.5;
+  var ballRadius = 10;
+  var ballColor = "#0095DD"
+
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width-paddleWidth) / 2;
+
+  var rightPressed = false;
+  var leftPressed = false;
+
+  function getRandomColor() {
+    ballColor = '#' + (0x1000000 + Math.floor(Math.random() * 0x1000000)).toString(16).substr(1);
+  }
+
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = ballColor;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+      dy = -dy;
+      getRandomColor();
+    }
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+      getRandomColor();
+    }
+  }
+
+  setInterval(draw, 10)
